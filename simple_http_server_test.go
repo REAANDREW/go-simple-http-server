1c54f946e30f2a5ab227e46d9deb9869692f69a3
REAANDREW <email@andrewrea.co.uk>
2015-02-02 08:04:13 +0000

    Support POST

--- simple_http_server_test.go
+++ simple_http_server_test.go
@@ -1,6 +1,7 @@
 package go_Simple_Http_Server
 
 import (
+	"fmt"
 	"github.com/franela/goblin"
 	"github.com/stretchr/testify/assert"
 	"io"
@@ -13,30 +14,45 @@ func Test_SimpleHttpServer(t *testing.T) {
 	g := goblin.Goblin(t)
 
 	g.Describe("SimpleHttpServer", func() {
+
+		var (
+			port   int
+			host   string
+			url    func(path string) string
+			server SimpleHttpServer
+		)
+
+		g.Before(func() {
+			port = 5000
+			host = "127.0.0.1"
+			url = func(path string) string {
+				return fmt.Sprintf("http://%s:%d%s", host, port, path)
+			}
+			server = NewSimpleHttpServer(port, host)
+		})
+
 		g.It("Supports GET", func() {
-			server := NewSimpleHttpServer(5000, "127.0.0.1")
 			server.Get("/", func(w http.ResponseWriter, r *http.Request) {
 				io.WriteString(w, "Hello world!")
 			})
 			server.Start()
-			resp, _ := http.Get("http://127.0.0.1:5000")
+			resp, _ := http.Get(url("/"))
 			assert.Equal(t, http.StatusOK, resp.StatusCode)
 			server.Stop()
-			_, err := http.Get("http://127.0.0.1:5000")
+			_, err := http.Get(url("/")
 			assert.True(t, err != nil)
 		})
 
 		g.It("Supports POST", func() {
-			server := NewSimpleHttpServer(5000, "127.0.0.1")
 			server.Post("/", func(w http.ResponseWriter, r *http.Request) {
 				io.WriteString(w, "Hello world!")
 			})
 			server.Start()
 			body := strings.NewReader("{}")
-			resp, _ := http.Post("http://127.0.0.1:5000", "application/json", body)
+			resp, _ := http.Post(url("/"), "application/json", body)
 			assert.Equal(t, http.StatusOK, resp.StatusCode)
 			server.Stop()
-			_, err := http.Get("http://127.0.0.1:5000")
+			_, err := http.Post(url("/"), "application/json", body)
 			assert.True(t, err != nil)
 		})
 	})
